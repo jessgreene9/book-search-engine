a418fb9e7e312136d7d48e27cf1ce2e1234fd079
Jess Greene <jess.greene9@gmail.com>
2021-07-22 21:18:27 -0400

    fixing remove book mutation

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -14,7 +14,7 @@ const SavedBooks = () => {
   const { loading, data } = useQuery(GET_USER);
   const userData = data?.me || {};
   
-  const removeBook = useMutation(REMOVE_BOOK); 
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
   
 
